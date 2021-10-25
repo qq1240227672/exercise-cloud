890690470d31512c4256039986ea42ba4aa0e1af
maofulong <maofulong@pccpa.cn>
2021-10-25 17:19:20 +0800

    test(test):the first commit

---
+++ exercise-server/src/main/java/org/fulong/server/listener/RegistryServerApplication.java
@@ -0,0 +1,17 @@
+package org.fulong.server.listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author maofulong
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServerApplication.class, args);
+    }
+}
+
